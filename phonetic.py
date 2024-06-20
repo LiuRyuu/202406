1a5df299a6d5ae43727b4809ac781f914be5634d
LiuRyuu <104825407+LiuRyuu@users.noreply.github.com>
2024-06-20 19:50:05 +0800

    Update phonetic.py

--- phonetic.py
+++ phonetic.py
@@ -1,43 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+def read ( word ):
+    url = f'https://dict.revised.moe.edu.tw/search.jsp?md=1&word={word}#searchL'
 
-def crawl_website(keyword):
-    if keyword == '國文':
-        url = 'https://stv.naer.edu.tw/learning/junior.jsp?cat=48'
-    elif keyword == '英文':
-        url = 'https://stv.naer.edu.tw/learning/junior.jsp?cat=49'
-    elif keyword == '數學':
-        url = 'https://stv.naer.edu.tw/learning/junior.jsp?cat=12'
-    elif keyword == '歷史':
-        url = 'https://stv.naer.edu.tw/learning/junior.jsp?cat=51'
-    elif keyword == '地理':
-        url = 'https://stv.naer.edu.tw/learning/junior.jsp?cat=52'
-    elif keyword == '公民':
-        url = 'https://stv.naer.edu.tw/learning/junior.jsp?cat=53'
-    elif keyword == '生物':
-        url = 'https://stv.naer.edu.tw/learning/junior.jsp?cat=54'
-    elif keyword == '理化':
-        url = 'https://stv.naer.edu.tw/learning/junior.jsp?cat=55'
-    elif keyword == '地球科學':
-        url = 'https://stv.naer.edu.tw/learning/junior.jsp?cat=56'
-    else:
-        return '沒有這個科目'
-
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    tab_content = soup.find('div', class_='tab-content')
+    html = requests.get( url )
+    bs = BeautifulSoup(html.text,'lxml')
+    data = bs.find('table', id='searchL')
     try:
-      if tab_content:
-          links = tab_content.find_all('a', href=True)
-          for link in links:
-              title = link.find_previous('p', class_='resource-title')
-              if title:
-                  title_text = title.text.strip()
-                  href = urljoin(url, link['href'])
-                  print(f' [{title_text}] -> ')
-                  print(f'  {href}')
-      return f'以上是科目: {keyword} 的相關教材影片'           
+        row = data.find_all('tr')[2]
+        chinese = row.find('cr').text
+        phones = row.find_all('code')
+        phone = [e.text for e in phones]
+        s = " ".join( phone )
+        # s = row.find('sub')
+        return chinese + ' -> ' + s 
     except:
-      return '沒有這個科目關教材影片'
+        return '查無此字' 
